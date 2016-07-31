400a7b7376d066049795c029cf5de06ce5b9ffeb
OHTSUKA Ko-hei <kochizufan@gmail.com>
2016-08-01 07:25:10 +0900

    方角追加

--- deviceorientation/js/index.js
+++ deviceorientation/js/index.js
@@ -19,10 +19,14 @@
     var gamma = event.gamma;
     // Z軸
     var alpha = event.alpha;
+
+    var heading = compassHeading( alpha, beta, gamma );
+
     var html = "";
     html += "X回転 : " + beta + "<br>";
     html += "Y回転 : " + gamma + "<br>";
-    html += 'Z回転 : ' + alpha;
+    html += 'Z回転 : ' + alpha + "<br>";
+    html += '方角 : ' + heading + "<br>";
     $("#debug").html(html);
 
     $zo.css({
@@ -30,4 +34,37 @@
       "transform": "rotateX(" + (180 + beta) + "deg) rotateY(" + (180 + gamma) + "deg) rotateZ(" + alpha + "deg)"
     })
   }
+var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+function compassHeading( alpha, beta, gamma ) {
+
+  var _x = beta  ? beta  * degtorad : 0; // beta value
+  var _y = gamma ? gamma * degtorad : 0; // gamma value
+  var _z = alpha ? alpha * degtorad : 0; // alpha value
+
+  var cX = Math.cos( _x );
+  var cY = Math.cos( _y );
+  var cZ = Math.cos( _z );
+  var sX = Math.sin( _x );
+  var sY = Math.sin( _y );
+  var sZ = Math.sin( _z );
+
+  // Calculate Vx and Vy components
+  var Vx = - cZ * sY - sZ * sX * cY;
+  var Vy = - sZ * sY + cZ * sX * cY;
+
+  // Calculate compass heading
+  var compassHeading = Math.atan( Vx / Vy );
+
+  // Convert compass heading to use whole unit circle
+  if( Vy < 0 ) {
+    compassHeading += Math.PI;
+  } else if( Vx < 0 ) {
+    compassHeading += 2 * Math.PI;
+  }
+
+  return compassHeading * ( 180 / Math.PI ); // Compass Heading (in degrees)
+
+}
+
 })();
