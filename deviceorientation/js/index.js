75e0e90efde1317918b8570be434d4afa57af1e6
OHTSUKA Ko-hei <kochizufan@gmail.com>
2016-08-02 03:51:26 +0900

    gyronorm利用

--- deviceorientation/js/index.js
+++ deviceorientation/js/index.js
@@ -4,14 +4,43 @@
 
   $(function () {
     $zo = $("#zo");
-    window.addEventListener("deviceorientation", deviceorientationHandler);
+
+    var gn = new GyroNorm();
+
+    gn.init().then(function(){
+      gn.start(deviceorientationHandler);//function(data){
+        // Process:
+        // data.do.alpha    ( deviceorientation event alpha value )
+        // data.do.beta     ( deviceorientation event beta value )
+        // data.do.gamma    ( deviceorientation event gamma value )
+        // data.do.absolute ( deviceorientation event absolute value )
+
+        // data.dm.x        ( devicemotion event acceleration x value )
+        // data.dm.y        ( devicemotion event acceleration y value )
+        // data.dm.z        ( devicemotion event acceleration z value )
+
+        // data.dm.gx       ( devicemotion event accelerationIncludingGravity x value )
+        // data.dm.gy       ( devicemotion event accelerationIncludingGravity y value )
+        // data.dm.gz       ( devicemotion event accelerationIncludingGravity z value )
+
+        // data.dm.alpha    ( devicemotion event rotationRate alpha value )
+        // data.dm.beta     ( devicemotion event rotationRate beta value )
+        // data.dm.gamma    ( devicemotion event rotationRate gamma value )
+      //});
+    }).catch(function(e){
+      // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+    });
+
+    //window.addEventListener("deviceorientation", deviceorientationHandler);
+
   });
 
   /**
    *
    * @param event
    */
-  function deviceorientationHandler(event) {
+  function deviceorientationHandler(data) {
+    var event = data.do;
     //ジャイロセンサー情報取得
     // X軸
     var beta = event.beta;
