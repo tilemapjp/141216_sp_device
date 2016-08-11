ababb95a7da710dc0ef103ccc53c5ed7615d3112
OHTSUKA Ko-hei <kochizufan@gmail.com>
2016-08-11 17:46:27 +0900

    0点修正

--- deviceorientation/js/index.js
+++ deviceorientation/js/index.js
@@ -95,11 +95,11 @@
     var camXY = camEuler.toVector3();
 
     //方角
-    var heading = -1 * (camXY.z + Math.PI) / degtorad;
+    var heading = (Math.PI - camXY.z) / degtorad;
     //俯角仰角
-    var pitch   = -1 * camXY.x / degtorad;
+    var pitch   = -camXY.x / degtorad;
     //水平角
-    var roll    = -1 * camXY.y / degtorad;
+    var roll    = (Math.PI - camXY.y) / degtorad;
 
     var html = "α:" + alpha + ",β:" + beta + ",γ:" + gamma + "<br>";
     html += "方角 : "   + heading + "<br>";
